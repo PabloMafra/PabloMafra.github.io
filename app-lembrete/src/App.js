5d2530f5262d518bd06b302be2e5f15b5063230a
Pablo <pablomafra_8@hotmail.com>
2023-04-11 16:41:06 -0300

    testando ghp

---
+++ app-lembrete/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Lembrete from './components/lembrete/index.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Lembrete/>
+    </div>
+  );
+}
+
+export default App;
