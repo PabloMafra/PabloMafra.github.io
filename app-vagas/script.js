990d7cf3003784f5310cb2915dcd6b41ab919053
Pablo <pablomafra_8@hotmail.com>
2023-04-19 20:41:24 -0300

    testando safari

--- app-vagas/script.js
+++ app-vagas/script.js
@@ -1,53 +1,7 @@
-// const vagas = document.querySelector('.vagas');
-// const vagasContainer = document.querySelector('.vagas-container');
-// let isDown = false;
-// let startX;
-// let scrollLeft;
-
-// vagas.addEventListener('mousedown', e => {
-//   isDown = true;
-//   startX = e.pageX - vagas.offsetLeft;
-//   scrollLeft = vagas.scrollLeft;
-// });
-
-// vagas.addEventListener('touchstart', e => {
-//   isDown = true;
-//   startX = e.touches[0].pageX - vagas.offsetLeft;
-//   scrollLeft = vagas.scrollLeft;
-// });
-
-// vagas.addEventListener('mouseleave', () => {
-//   isDown = false;
-// });
-
-// vagas.addEventListener('mouseup', () => {
-//   isDown = false;
-// });
-
-// vagas.addEventListener('touchend', () => {
-//   isDown = false;
-// });
-
-// vagas.addEventListener('mousemove', e => {
-//   if (!isDown) return;
-//   e.preventDefault();
-//   const x = e.pageX - vagas.offsetLeft;
-//   const walk = (x - startX) * 3; // Multiplicando por 3 para aumentar a sensibilidade do movimento
-//   vagas.scrollLeft = scrollLeft - walk;
-// });
-
-// vagas.addEventListener('touchmove', e => {
-//   if (!isDown) return;
-//   e.preventDefault();
-//   const x = e.touches[0].pageX - vagas.offsetLeft;
-//   const walk = (x - startX) * 3;
-//   vagas.scrollLeft = scrollLeft - walk;
-// });
-
 const red = document.getElementById('red');
 const blue = document.getElementById('blue');
 const yellow = document.getElementById('yellow');
-const green = document.getElementById('green');
+const black = document.getElementById('black');
 const orange = document.getElementById('orange');
 const white = document.getElementById('white');
 
@@ -56,7 +10,7 @@ function chooseRed(){
     red.style.display = "block"
     yellow.style.display = "none";
     blue.style.display = "none";
-    green.style.display = "none";
+    black.style.display = "none";
     orange.style.display = "none";
     white.style.display = "none";
 }
@@ -65,7 +19,7 @@ function chooseBlue(){
     red.style.display = "none"
     yellow.style.display = "none";
     blue.style.display = "block";
-    green.style.display = "none";
+    black.style.display = "none";
     orange.style.display = "none";
     white.style.display = "none";
 }
@@ -74,16 +28,16 @@ function chooseYellow(){
     red.style.display = "none"
     yellow.style.display = "block";
     blue.style.display = "none";
-    green.style.display = "none";
+    black.style.display = "none";
     orange.style.display = "none";
     white.style.display = "none";
 }
 
-function chooseGreen(){
+function chooseBlack(){
     red.style.display = "none"
     yellow.style.display = "none";
     blue.style.display = "none";
-    green.style.display = "block";
+    black.style.display = "block";
     orange.style.display = "none";
     white.style.display = "none";
 }
@@ -92,7 +46,7 @@ function chooseOrange(){
     red.style.display = "none"
     yellow.style.display = "none";
     blue.style.display = "none";
-    green.style.display = "none";
+    black.style.display = "none";
     orange.style.display = "block";
     white.style.display = "none";
 }
@@ -101,7 +55,7 @@ function chooseWhite(){
     red.style.display = "none"
     yellow.style.display = "none";
     blue.style.display = "none";
-    green.style.display = "none";
+    black.style.display = "none";
     orange.style.display = "none";
     white.style.display = "block";
 }
